66c29ecf2eb8deb1884dbabf32b5bd76b5575c72
SRC0747 <RjSrc@0747>
2021-08-15 13:45:45 +0530

    added: Calculate Daily Employee wage.

--- src/com/employeewagemulticompany/EmployeeWageBuilder.java
+++ src/com/employeewagemulticompany/EmployeeWageBuilder.java
@@ -2,12 +2,21 @@ package com.employeewagemulticompany;
 
 public class EmployeeWageBuilder {
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee wage Building problem for multiple company.");
+        System.out.println("Welcome to Employee Wage Computation Problem.");
         int IS_FULL_TIME=1;
+        int EMP_RATE_PER_HOUR=20;
+        int empHrs=0;
+        int empWage=0;
         double empCheck = Math.floor(Math.random()*10)%2;
-        if (empCheck == IS_FULL_TIME)
+        if (empCheck == IS_FULL_TIME) {
             System.out.println("Employee is Present.");
-        else
+            empHrs=8;
+        }
+        else {
             System.out.println("Employee is Absent.");
+            empHrs=0;
+        }
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Employee Wage is:"+empWage);
     }
 }
